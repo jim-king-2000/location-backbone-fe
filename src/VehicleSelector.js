7f644b51d778955f7b0e260a072400646b7666c5
Jim <jim_king_2000@163.com>
2019-06-26 15:13:58 +0800

    init code

---
+++ src/VehicleSelector.js
@@ -0,0 +1,13 @@
+import { Box, CheckBox } from 'grommet';
+import { observer } from 'mobx-react';
+
+export const VehicleSelector = observer(({ vehicles, onChange, ...props }) => (
+  <Box {...props}>
+    {vehicles.map(v =>
+      <CheckBox
+        key={v.thingId}
+        label={v.thingName}
+        checked={!!v.enabled}
+        onChange={onChange} />)}
+  </Box>
+));
