6594609f2a18ca0a60f9ea66f215c2fdfa303a1c
Jim <jim_king_2000@163.com>
2019-07-02 10:01:40 +0800

    add TrackVisualizer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export * from './src/TrackPlayer';
 export * from './src/DateTimePicker';
 export * from './src/DateTimeRangePicker';
+export * from './src/TrackVisualizer';
 export * from './src/VehicleSelector';
