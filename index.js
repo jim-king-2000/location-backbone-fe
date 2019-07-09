338f2fbef6370874aca6ea2a9d77ce1dd36277aa
Jim <jim_king_2000@163.com>
2019-07-09 21:19:00 +0800

    refactory

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 export * from './src/TrackPlayer';
 export * from './src/DateTimePicker';
 export * from './src/DateTimeRangePicker';
-export * from './src/PluginZoom';
 export * from './src/TrackVisualizer';
 export * from './src/VehicleSelector';
